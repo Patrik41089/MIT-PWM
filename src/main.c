dfaa7ac4b946ef53f6e0491248a738d22d05cc12
Patrik Radvák <rad41089@spseol.cz>
2024-03-25 14:06:00 +0100

    vylepšení 25.3.

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 #include "uart1.h"
 #include "daughterboard.h"
 
+
 void init(void)
 {
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
@@ -15,21 +16,30 @@ void init(void)
     GPIO_Init(PWM_G_PORT, PWM_R_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
     GPIO_Init(PWM_B_PORT, PWM_R_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
 
+    GPIO_Init(S1_PORT, S1_PIN, GPIO_MODE_IN_PU_NO_IT);
+    GPIO_Init(S2_PORT, S2_PIN, GPIO_MODE_IN_PU_NO_IT);
+    GPIO_Init(S3_PORT, S3_PIN, GPIO_MODE_IN_PU_NO_IT);
+
+    GPIO_Init(LED1_PORT, LED1_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_Init(LED4_PORT, LED4_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_Init(LED2_PORT, LED2_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+
     init_milis();
     init_uart1();
 
     TIM2_DeInit();
-    TIM2_TimeBaseInit(TIM2_PRESCALER_16, 10e-3 -1);
+    TIM2_TimeBaseInit(TIM2_PRESCALER_16, 100);
     TIM2_OC1Init( //konfigurace output channel1
         TIM2_OCMODE_PWM1, //mod PWM1
          TIM2_OUTPUTSTATE_ENABLE, //povolím
-          1000, //nastavuju šířku impulzu
+          50, //nastavuju šířku impulzu
           TIM2_OCPOLARITY_HIGH); //nastavení polarity
-    TIM2_OCInit(TIM2_OCMODE_PWM2, TIM2_OUTPUTSTATE_ENABLE, 5000, TIM2_OCPOLARITY_HIGH);
-    TIM2_OCInit(TIM2_OCMODE_PWM3, TIM2_OUTPUTSTATE_ENABLE, 9000, TIM2_OCPOLARITY_HIGH);
-    TIM2_OC1PrescalerConfig(ENABLE);
-    TIM2_OC2PrescalerConfig(ENABLE);
-    TIM2_OC3PrescalerConfig(ENABLE);
+    TIM2_OC2Init(TIM2_OCMODE_PWM1, TIM2_OUTPUTSTATE_ENABLE, 10, TIM2_OCPOLARITY_HIGH);
+    TIM2_OC3Init(TIM2_OCMODE_PWM1, TIM2_OUTPUTSTATE_ENABLE, 90, TIM2_OCPOLARITY_HIGH);
+
+    TIM2_OC1PreloadConfig(ENABLE);
+    TIM2_OC2PreloadConfig(ENABLE);
+    TIM2_OC3PreloadConfig(ENABLE);
     TIM2_Cmd(ENABLE);
 }
 
@@ -38,17 +48,41 @@ int main(void)
 {
   
     uint32_t time = 0;
+    uint8_t r = 1, g = 1, b = 1;
+    bool S1memory  = false;
+    bool S3memory = false;
+    uint8_t color = 1;
 
     init();
 
     while (1) {
         if (milis() - time > 333 ) { 
             time = milis();
-            TIM2_SetCompare1(4000);
+
+            if (PUSH(S1) && ! S1memory){
+                color++;
+                if (color > 3){
+                    color = 1;
+                }
+                LOW(LED1);
+                LOW(LED4);
+                LOW(LED2);
+                if (color == 1) HIGH(LED1);
+                if (color == 2) HIGH(LED4);
+                if (color == 3) HIGH(LED2);
+            }
+            }
+            S1memory = PUSH(S1);
+
+            if (! PUSH(S3) && S3memory){
+
+            }
+            S3memory = PUSH(S3);
+            TIM2_SetCompare1(r);
+            TIM2_SetCompare2(g);
+            TIM2_SetCompare3(b);
         }
 
-    }
-}
 }
 /*-------------------------------  Assert -----------------------------------*/
 #include "__assert__.h"
